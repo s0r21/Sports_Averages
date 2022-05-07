335fbbdb7047d16c3e51d0df889512f1644440e1
s0r21 <sorena.talebi@gmail.com>
2022-05-07 00:40:55 -0400

    Remove the now ignored directory "OLD"

--- OLD/Previous Code/ARMA_Basketball/packages.py
+++
@@ -1,6 +0,0 @@
-# packages used
-
-import pandas as pd
-import numpy as np
-import warnings
-from statsmodels.tsa.arima.model import ARIMA
--- OLD/Previous Code/Log_Reg_Script_Outdated/packages.py
+++
@@ -1,6 +0,0 @@
-# Script for all the packages
-
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
